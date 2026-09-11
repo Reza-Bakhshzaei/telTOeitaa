0e17f20794bd991f66e4027a299c2b21e4dbc54e
Reza Bakhsh zaei <99674430+Reza-Bakhshzaei@users.noreply.github.com>
2026-09-11 15:41:18 +0330

    12

--- eitaabot.py
+++ eitaabot.py
@@ -1,6 +1,6 @@
 from rubika import Bot
+bot = Bot("boot", "hleftowpbgadchydlxwixmqhthvoesto")
 
 def upload_file(chat_id, path):
-    bot = Bot("boot", "hleftowpbgadchydlxwixmqhthvoesto")
     bot.sendVideo( chat_id, path)
     return True
