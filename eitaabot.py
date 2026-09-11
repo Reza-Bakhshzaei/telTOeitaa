cdff16bc85a94b88a9607bb9b361a2f8e2cb26e1
Reza Bakhsh zaei <99674430+Reza-Bakhshzaei@users.noreply.github.com>
2026-09-11 15:11:05 +0330

    6

--- eitaabot.py
+++ eitaabot.py
@@ -6,8 +6,8 @@ auth = "u0GOux0964339f923160b7fd0addb826"
 
 async def upload_file(chat_id, path):
     try:
-        bot = await Bot("boot", auth)
-        await bot.sendVideo( chat_id, path)
+        bot = Bot("boot", auth)
+        bot.sendVideo( chat_id, path)
         return True
     except Exception as e:
         return str(e)
